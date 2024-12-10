85da929cec6dd7b985caf564af881ac5bdc5bdeb
dev-kart <kartz2dev@gmail.com>
2024-12-10 11:15:22 +0530

    recursion code

---
+++ recursion_gcd.c
@@ -0,0 +1,44 @@
+// // without recursion
+// #include <stdio.h>
+// int gcd(int a, int b)
+// {
+//     while (b != 0)
+//     {
+//         int temp = b;
+//         b = a % b;
+//         a = temp;
+//     }
+//     return a;
+// }
+
+// int main()
+// {
+//     int num1, num2;
+//     printf("Enter two integers: ");
+//     scanf("%d %d", &num1, &num2);
+
+//     printf("GCD of %d and %d is %d\n", num1, num2, gcd(num1, num2));
+//     return 0;
+// }
+
+//with recursion
+#include <stdio.h>
+
+int gcd(int a, int b)
+{
+    if (b == 0)
+        return a;
+    return gcd(b, a % b);
+}
+
+int main()
+{
+    int num1, num2;
+
+    printf("Enter two integers: ");
+    scanf("%d %d", &num1, &num2);
+
+    printf("GCD of %d and %d is %d\n", num1, num2, gcd(num1, num2));
+
+    return 0;
+}
